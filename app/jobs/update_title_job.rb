0775bcf14e79abfbf72f6b78b41b4a02c970d239
Benjamin Wireman <wiremanbenjamin@gmail.com>
2020-11-16 14:24:46 -0600

    Finished add title in background job

--- app/jobs/update_title_job.rb
+++ app/jobs/update_title_job.rb
@@ -1,8 +1,11 @@
+require 'open-uri'
+require 'nokogiri'
+
 class UpdateTitleJob < ApplicationJob
   queue_as :default
 
   def perform(short_url_id)
     @shortcode = Shortcode.find(short_url_id)
-    @shortcode.update(title: @shortcode.original_url)
+    @shortcode.update(title: Nokogiri::HTML.parse(open(@shortcode.original_url)).title)
   end
 end
--- spec/jobs/update_title_job_spec.rb
+++ spec/jobs/update_title_job_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe UpdateTitleJob, type: :job do
-  pending "add some examples to (or delete) #{__FILE__}"
+  include ActiveJob::TestHelper
+
+  let(:short_url) { Shortcode.create(original_url: "https://www.beenverified.com/faq/") }
+  let(:job) { UpdateTitleJob.perform_later(short_url.id) }
+
+  it "updates the title" do
+    expect(short_url.title).to be_nil
+    perform_enqueued_jobs { job }
+    short_url.reload
+    expect(short_url.title).to eq("Frequently Asked Questions | BeenVerified")
+  end
 end
